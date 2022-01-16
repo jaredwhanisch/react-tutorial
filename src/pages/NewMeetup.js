9d903922fd5967a5a4b008c5f549a26e89a5d2e3
Jared Hanisch <jared.hanisch@gmail.com>
2022-01-16 13:00:02 -0600

    Finished React meetup application tutorial.

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+  
   function addMeetupHandler(meetupData) {
     fetch(
-      "https://react-getting-started-b3176-default-rtdb.firebaseio.com/meetups.json",
+      'https://react-getting-started-b3176-default-rtdb.firebaseio.com/meetups.json',
       {
         method: 'POST',
         body: JSON.stringify(meetupData),
@@ -11,7 +15,9 @@ function NewMeetupPage() {
           'Content-Type': 'application/json'
         }
       }
-    );
+    ).then(() => {
+      navigate('/', {replace: true});
+    });
   }
 
   return (
